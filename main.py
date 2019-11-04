39b459de57fea0dfcb907576b33910458b02f770
Amy Hudspith <amyhudspith999@gmail.com>
2019-11-04 13:18:59 +0000

    Basic functionailty added: - Read in both csvs - Convert csvs to data frames

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import pandas as pd
 import numpy as np
 from scipy.sparse.linalg import svds
 
+
+def recommend_book():
+    print("Recommended book is: ")
+
+
+raw_books = pd.read_csv("data/books.csv")
+print(raw_books.head())
+print("---------------")
+print(raw_books.info())
+
+raw_ratings = pd.read_csv("data/ratings.csv")
+print(raw_ratings.head())
+print("---------------")
+print(raw_ratings.info())
+
+recommend_book()
